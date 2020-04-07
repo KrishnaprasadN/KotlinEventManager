639339bed7599dda95034faab953e457513ff654
Rakshith Raj <rakshith.r@CORP.SYSTECHMNG.NET>
2020-04-07 12:48:53 +0530

    Some coroutine functionality added.

--- app/src/main/java/com/devteam/eventmanager/ui/Registration/RegisterActivity.kt
+++ app/src/main/java/com/devteam/eventmanager/ui/Registration/RegisterActivity.kt
@@ -26,27 +26,45 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.storage.OnProgressListener
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), CoroutineScope {
     private lateinit var bind: ActivityRegisterBinding
     private var fileUri: Uri? = null
     lateinit var firebaseUser: FirebaseUser
+
+
+    //Scopes in Kotlin Coroutines are very useful because we need to cancel the background task as soon as the activity is destroyed
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job   // Dispatchers.Main + job + handler  <- one more way to handle Exception in activity scope
+    private lateinit var job: Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(bind.root)
         setSupportActionBar(toolbar)
         setTitle(R.string.profile)
+
+        init()
+
+    }
+
+    override fun onDestroy() {
+        job.cancel() // cancel the Job
+        super.onDestroy()
+    }
+
+    private fun init() {
+        job = Job()
+
         val auth = FirebaseAuth.getInstance()
         firebaseUser = auth.currentUser!!
-
         bind.name = auth.currentUser?.displayName
         bind.uri = auth.currentUser?.photoUrl
 
@@ -65,31 +83,41 @@ class RegisterActivity : AppCompatActivity() {
      */
     fun uploadImage(view: View) {
         ImagePicker.with(this)
-            .crop()                    //Crop image(Optional), Check Customization for more option
-            .compress(1024)            //Final image size will be less than 1 MB(Optional)
-            .maxResultSize(
-                1080,
-                1080
-            )    //Final image resolution will be less than 1080 x 1080(Optional)
-            .start { resultCode, data ->
-                if (resultCode == Activity.RESULT_OK) {
-                    //Image Uri will not be null for RESULT_OK
-                    fileUri = data?.data
-                    bind.profilePic.setImageURI(fileUri)
+                .crop()                    //Crop image(Optional), Check Customization for more option
+                .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(
+                        1080,
+                        1080
+                )    //Final image resolution will be less than 1080 x 1080(Optional)
+                .start { resultCode, data ->
+                    if (resultCode == Activity.RESULT_OK) {
+                        //Image Uri will not be null for RESULT_OK
+                        fileUri = data?.data
+                        bind.profilePic.setImageURI(fileUri)
 
-                    //You can get File object from intent
-                    // val file: File? = ImagePicker.getFile(data)
+                        //You can get File object from intent
+                        // val file: File? = ImagePicker.getFile(data)
 
-                    //You can also get File Path from intent
-                    //  val filePath:String? = ImagePicker.getFilePath(data)
+                        //You can also get File Path from intent
+                        //  val filePath:String? = ImagePicker.getFilePath(data)
 
-                } else if (resultCode == ImagePicker.RESULT_ERROR) {
-                    Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                    } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                        Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                    }
                 }
-            }
 
+    }
+
+    //Exception Handling in Kotlin Coroutines
+    val handler = CoroutineExceptionHandler { _, exception ->
+        bind.registeringUser = false
+        Toast.makeText(
+                this@RegisterActivity,
+                "register fail",
+                Toast.LENGTH_LONG
+        ).show()
     }
 
     /**
@@ -97,26 +125,27 @@ class RegisterActivity : AppCompatActivity() {
      */
     fun register(view: View) {
         bind.name?.let {
-            val job = GlobalScope.launch {
+            launch(Dispatchers.Main + handler) {
                 bind.registeringUser = true
-                fileUri?.let {
-                    val updateProifleStatus = async { updateProifleUri() }
-                    val updateProifleDisplayStatus = async { updateProifleDisplayName() }
-                    if (updateProifleStatus.await() && updateProifleDisplayStatus.await()) {
-                        goToHomePage()
+                val updateProifleStatus = async { updateProifleUri() }
+                val updateProifleDisplayNameStatus = async { updateProifleDisplayName() }
+                if (updateProifleStatus.await() && updateProifleDisplayNameStatus.await()) {
+                       // goToHomePage()
+                        Toast.makeText(
+                                this@RegisterActivity,
+                                "register success",
+                                Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                                this@RegisterActivity,
+                                "register fail",
+                                Toast.LENGTH_LONG
+                        ).show()
                     }
-
                     bind.registeringUser = false
-
-                } ?: launch {
-                    val updateProifleDisplay = async { updateProifleDisplayName() }
-                    if (updateProifleDisplay.await()) {
-                        goToHomePage()
-                    }
-                    bind.registeringUser = false
-
-                }
             }
+
         } ?: bind.userName.setError(getString(R.string.enter_vaild_phone));
 
 
@@ -128,7 +157,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun goToHomePage() {
         SharedPreferences().loogedIn(baseContext)
         val intent =
-            Intent(this@RegisterActivity, ExperimentMainActivity::class.java)
+                Intent(this@RegisterActivity, ExperimentMainActivity::class.java)
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
         finish()
@@ -141,8 +170,8 @@ class RegisterActivity : AppCompatActivity() {
     private suspend fun updateProifleDisplayName(): Boolean {
         if (!firebaseUser.displayName.equals(bind.name)) {
             val profileUpdates = UserProfileChangeRequest.Builder()
-                .setDisplayName(bind.name)
-                .build()
+                    .setDisplayName(bind.name)
+                    .build()
             return updateProfile(profileUpdates)
         } else return true
     }
@@ -151,37 +180,39 @@ class RegisterActivity : AppCompatActivity() {
      * Update user photo uri to firebase
      */
     private suspend fun updateProifleUri(): Boolean {
-        val uri = uploadProfilePic()
-        uri?.let {
-            val profileUpdates = UserProfileChangeRequest.Builder()
-                .setPhotoUri(uri)
-                .build()
-            return updateProfile(profileUpdates)
-        } ?: return false
+        return withContext(Dispatchers.IO) {
+            fileUri?.let {
+                val uri = uploadProfilePic()
+                uri?.let {
+                    val profileUpdates = UserProfileChangeRequest.Builder()
+                            .setPhotoUri(uri)
+                            .build()
+                     updateProfile(profileUpdates)
+                } ?:  false
+            } ?:  true
+        }
     }
 
     /**
      * upload image to firebase and get download uri
      */
     private suspend fun uploadProfilePic(): Uri? {
-        return suspendCoroutine { cont ->
-            FirebaseStorageUtils().uploadProfilePhoto(fileUri, OnFailureListener {
-                Toast.makeText(
-                    this@RegisterActivity,
-                    "Please try again",
-                    Toast.LENGTH_LONG
-                ).show()
-                cont.resume(null)
-            }, OnSuccessListener {
-                it.storage.getDownloadUrl().addOnSuccessListener(OnSuccessListener<Uri> { uri ->
-                    cont.resume(uri)
-                })
+        // call withContext(Dispatchers.IO) to create a block that runs on the IO thread pool.
+        return withContext<Uri?>(Dispatchers.IO){
+             suspendCoroutine { cont ->
+                FirebaseStorageUtils().uploadProfilePhoto(fileUri, OnFailureListener {
+                    cont.resume(null)
+                }, OnSuccessListener {
+                    it.storage.getDownloadUrl().addOnSuccessListener(OnSuccessListener<Uri> { uri ->
+                        cont.resume(uri)
+                    })
 
-            }, OnProgressListener {
-                val progress = (100.0 * it.bytesTransferred / it.totalByteCount)
-                //bind.progressBar.message.text = "Uploaded "+progress+"%"
-                bind.progressBar.progress = progress.toInt()
-            })
+                }, OnProgressListener {
+                    val progress = (100.0 * it.bytesTransferred / it.totalByteCount)
+                    //bind.progressBar.message.text = "Uploaded "+progress+"%"
+                    bind.progressBar.progress = progress.toInt()
+                })
+            }
         }
     }
 
@@ -189,21 +220,21 @@ class RegisterActivity : AppCompatActivity() {
      * update user profile to firebase
      */
     suspend fun updateProfile(
-        profileUpdates: UserProfileChangeRequest
+            profileUpdates: UserProfileChangeRequest
     ): Boolean {
         return suspendCoroutine { cont ->
             run {
                 firebaseUser.updateProfile(profileUpdates)
-                    .addOnCompleteListener(object : OnCompleteListener<Void> {
-                        override fun onComplete(task: Task<Void>) {
-                            if (task.isSuccessful) {
-                                cont.resume(true)
-                            } else {
-                                cont.resume(false)
+                        .addOnCompleteListener(object : OnCompleteListener<Void> {
+                            override fun onComplete(task: Task<Void>) {
+                                if (task.isSuccessful) {
+                                    cont.resume(true)
+                                } else {
+                                    cont.resume(false)
+                                }
                             }
-                        }
 
-                    })
+                        })
             }
         }
 
